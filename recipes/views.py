aca7ae3df9bd09d3c7f789b95f46ad7afbab349d
Pedro Augusto Guimarães Santos <pedroaugustoguimaraesantos@gmail.com>
2025-11-04 22:46:08 -0300

    Exemplo de template temp

--- recipes/views.py
+++ recipes/views.py
@@ -1,13 +1,17 @@
+from os import name
+
 from django.http import HttpResponse
 from django.shortcuts import render
 
 
 def home(request):
-    return HttpResponse("HOME 1")
+    return render(request, "global/home.html", context={
+        "name": "Pedro Augusto",
+    })
 
 
 def contato(request):
-    return HttpResponse("contato")
+    return render(request, 'me_apague/temp.hmtl')
 
 
 def sobre(request):
